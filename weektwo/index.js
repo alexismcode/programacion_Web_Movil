964abcd13f362be6df561379417a8a94419cba17
Alexis <alexismcode@gmail.com>
2024-05-16 21:24:21 -0500

    Create folder and files of week 2

---
+++ weektwo/index.js
@@ -0,0 +1,23 @@
+//let isBoolean = 21 % 2 == 0 ? "Es Correcto!" : "Es Incorrecto";
+
+//const myFruits = ["banana", "apple", "grape", 23]
+
+// for(let i = 1; i <= myFruits.length; i++){
+//     console.log(`Mi fruta favorita es ${myFuits[i]} y es deliciosa}`);
+// }
+
+// const new_fruits = myFruits.map((i) => i.replace(/a/g, "e") )
+
+//Funciones
+
+// function get_hello(name){
+//     console.log(`Bienvenido ${name}`)
+// }
+
+// Tipo Flecha
+// const get_hello2 = (name) => `Bienvenido ${name}`
+
+// console.log(get_hello2("Alexis"));
+
+// console.log(get_hello("Renny"));
+
